d726bc7907bc601ae21693f488fe5ddf234c0623
ilyasa1211 <muhammadilyasafadhlih@gmail.com>
2025-11-14 10:04:33 +0000

    Add: input

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,15 @@ enum SIZE
   H,
 };
 
+enum ACTION
+{
+  NONE,
+  MOVE_RIGHT,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  FREE_FALL,
+};
+
 void run(const int window_size[2]);
 void draw_shape(const std::vector<std::vector<bool>> &shape, int x, int y, const int width, const int height, const u_int8_t color[4], const int grid_size[2], const int window_size[2]);
 
@@ -36,14 +45,14 @@ int main()
 void run(const int window_size[2])
 {
   const std::vector<std::vector<std::vector<bool>>> shapes = {I, J, L, O, S, T, Z};
-  // apply random color
-  constexpr u_int8_t shape_color[4] = {200, 0, 0, 255};
+  // apply random color 
+  constexpr u_int8_t shape_color[4] = {200, 0, 0, 255}; // red
 
   // grid width and height
   constexpr int grid_size[2] = {20, 40};
 
   // shape spawn area width from the top of screen
-  constexpr int spawn_area_range[2] = {10, 20};
+  constexpr int spawn_area_range[2] = {5, 15};
 
   // get first and second shape
   const auto shape = shapes.at(GetRandomValue(0, shapes.size() - 1)); // example
@@ -61,12 +70,19 @@ void run(const int window_size[2])
       0,
   };
 
-  float move_interval_seconds = 1.f;
+  float move_interval_seconds = 0.4f;
   float accumulated_time_seconds = 0.0f;
+
+  ACTION action = ACTION::NONE;
 
   // game loop
   while (!WindowShouldClose())
   {
+    if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) action = ACTION::MOVE_LEFT;
+    if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) action = ACTION::MOVE_DOWN;
+    if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) action = ACTION::MOVE_RIGHT;
+    if (IsKeyPressed(KEY_SPACE)) action = ACTION::FREE_FALL;
+
     BeginDrawing();
     // get delta time if already one second then apply offset - 1 on y axis
 
@@ -75,13 +91,28 @@ void run(const int window_size[2])
 
     ClearBackground(BLACK);
 
-    DrawRectangle(100, 200, 100, 200, BLUE);
-
+    // main moving
     if (accumulated_time_seconds >= move_interval_seconds)
     {
       accumulated_time_seconds = 0.f;
       shape_pos_grid[POS::Y] += 1;
     }
+
+    switch (action)
+    {
+    case ACTION::MOVE_LEFT:
+      shape_pos_grid[POS::X] -= 1;
+      break;
+    case ACTION::MOVE_RIGHT:
+      shape_pos_grid[POS::X] += 1;
+      break;
+    case ACTION::MOVE_DOWN:
+      shape_pos_grid[POS::Y] += 1;
+      break;
+    default:
+      break;
+    }
+
 
     //
     // if already on bottom then use second shape and pick new shape for the third one
@@ -91,6 +122,8 @@ void run(const int window_size[2])
     draw_shape(shape, shape_pos_grid[POS::X], shape_pos_grid[POS::Y], shape_size_grid[SIZE::W], shape_size_grid[SIZE::H], shape_color, grid_size, window_size);
 
     EndDrawing();
+
+    action = ACTION::NONE;
   }
 }
 
